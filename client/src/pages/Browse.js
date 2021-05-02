b1473942486e9e05099465beac2064c2924cf642
Ivan Feng <feng.ivan.1235@gmail.com>
2021-05-01 22:13:40 -0400

    connected express to react

--- client/src/pages/Browse.js
+++ client/src/pages/Browse.js
@@ -1,9 +1,22 @@
 import React from 'react'
 
 const Browse = () => {
+
+    //code for fetch the trending anime for user to look at
+    const fetch_data = async () => {
+        try {
+            let response = await fetch('/api/browse')
+            let data = await response.text()
+            data = await JSON.parse(data)
+            console.log(data)
+        }
+        catch(err){
+            alert(err)
+        }
+    }
+    fetch_data()
     return (
         <div>
-
         </div>
     )
 }
--- server.js
+++ server.js
@@ -2,6 +2,22 @@
 
  const app = express()
 
- const port = 6000
+ const port = 5000
 
+ // code for fetchign from api 
+const fetch = require('node-fetch');
+
+const base_api_url = 'https://kitsu.io/api/edge'
+
+
+// endpoint to get data for browse page
+app.get('/api/browse', async (request, response) => {
+    let url = base_api_url + '/trending/anime'
+    const rawResp = await fetch(url)
+    const data = await rawResp.json()
+    response.send(data)
+    console.log(data)
+})
 app.listen(port, () => console.log(` Server started at port ${port}`))
+
+
