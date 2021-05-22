511eac4ebe844ce526211ba351bd00e290eaeb01
Ivan Feng <feng.ivan.1235@gmail.com>
2021-05-22 01:10:03 -0400

    removed useless import

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import Startpage from "./pages/Startpage.js";
 import Browse from "./pages/Browse.js";
 import Profile from "./pages/Profile.js";
-import Apollotest from "./pages/Apollotest.js";
 import Header from "./components/Header.js";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
