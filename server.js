e4a5f1ba78139a0bfe70d299703cf3ad93c0acb0
Ivan Feng <feng.ivan.1235@gmail.com>
2021-05-25 18:52:48 -0400

    starter code for connecting postgres to node

--- server.js
+++ server.js
@@ -9,4 +9,38 @@ app.listen(port, () => console.log(` Server started at port ${port}`));
 //auth0
 const path = require("path");
 
-const { auth } = require("express-openid-connect");
+const { Pool } = require("pg");
+const pool = new Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "postgres",
+  password: process.env.PASSWORD,
+  port: 5432,
+});
+
+pool.on("error", (err, client) => {
+  console.error("Error:", err);
+});
+
+const query = `
+SELECT *
+FROM dog 
+`;
+
+pool
+  .connect()
+  .then((client) => {
+    client
+      .query(query)
+      .then((res) => {
+        for (let row of res.rows) {
+          console.log(row);
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  })
+  .catch((err) => {
+    console.error(err);
+  });
