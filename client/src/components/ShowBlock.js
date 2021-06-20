ee85f345237d6d72f2af5f1274646f0dee099391
Ivan Feng <feng.ivan.1235@gmail.com>
2021-06-19 21:00:46 -0400

    Showblock can now send data to node

--- client/src/components/ShowBlock.js
+++ client/src/components/ShowBlock.js
@@ -10,7 +10,30 @@ import AddCircleIcon from '@material-ui/icons/AddCircle';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
+// axios for posting
+import axios from 'axios';
+
+import { useAuth0 } from '@auth0/auth0-react';
+
+// functino to handle passing data from showBlock to node, which then gets stored into the db
+function handleSubmitAdd(e, userData, showData) {
+  // authentication
+  e.preventDefault();
+  // dont think i need configs?
+  // let axiosConfig = {
+  //   headers: {
+  //     'Content-Type': 'application/json;',
+  //   },
+  // };
+  axios
+    .post('http://localhost:5000/api', { user: userData, show: showData })
+    .then((res) => console.log('success, dictionary sent,', res))
+    .catch((err) => {
+      console.log(err.response);
+    });
+}
 const ShowBlock = ({ data }) => {
+  const { user, isAuthenticated } = useAuth0();
   const [showAddButton, setShowAddButt] = useState(false);
   const useStyles = makeStyles({
     root: {
@@ -47,6 +70,11 @@ const ShowBlock = ({ data }) => {
               className={classes.addIcon}
               onMouseEnter={() => setShowAddButt(true)}
               onMouseLeave={() => setShowAddButt(false)}
+              onClick={
+                isAuthenticated
+                  ? (event) => handleSubmitAdd(event, user, data)
+                  : () => console.log('did not submit')
+              } // if user is logged in then add the show to db, if not dont add
             >
               <AddCircleIcon style={{ color: 'black' }} />
             </IconButton>
