a63b29e7cb551ea4277bae5ba107cf239337193d
Ivan Feng <feng.ivan.1235@gmail.com>
2021-06-19 21:02:21 -0400

    this shouldve been with the last commit. Node now has api endpoint for frontend to call

--- server.js
+++ server.js
@@ -1,25 +1,43 @@
-const express = require("express");
-
+const express = require('express');
+const logger = require('morgan');
 const app = express();
+const cors = require('cors');
 
 const port = 5000;
-require("dotenv").config();
+require('dotenv').config();
 app.listen(port, () => console.log(` Server started at port ${port}`));
 
-//auth0
-const path = require("path");
+// stuff for making post request from front end to node (this) work
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  })
+);
 
-const { Pool } = require("pg");
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// receiving body from react
+app.post('/api', (req, resp) => {
+  console.log(req.body);
+  resp.send('post request received from node');
+});
+
+const path = require('path');
+
+const { Pool } = require('pg');
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "postgres",
+  user: 'postgres',
+  host: 'localhost',
+  database: 'postgres',
   password: process.env.PASSWORD,
   port: 5432,
 });
 
-pool.on("error", (err, client) => {
-  console.error("Error:", err);
+pool.on('error', (err, client) => {
+  console.error('Error:', err);
 });
 
 const query = `
