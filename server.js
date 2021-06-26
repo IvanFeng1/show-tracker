d54f0334be02781722a51369a0cb14505c005bfc
Ivan Feng <feng.ivan.1235@gmail.com>
2021-06-25 23:35:35 -0400

    express server now stores showid with useremail in db

--- server.js
+++ server.js
@@ -19,12 +19,6 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// receiving body from react
-app.post('/api', (req, resp) => {
-  console.log(req.body);
-  resp.send('post request received from node');
-});
-
 const path = require('path');
 
 const { Pool } = require('pg');
@@ -40,25 +34,20 @@ pool.on('error', (err, client) => {
   console.error('Error:', err);
 });
 
-const query = `
-SELECT *
-FROM dog 
-`;
-
-pool
-  .connect()
-  .then((client) => {
-    client
-      .query(query)
-      .then((res) => {
-        for (let row of res.rows) {
-          console.log(row);
-        }
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-  })
-  .catch((err) => {
-    console.error(err);
+// receiving body from react
+app.post('/api', (req, resp) => {
+  console.log(req.body);
+  let req_body = req.body;
+  let user_data = req_body.user.name;
+  let show_id = req_body.showID;
+  let insert_query = {
+    text: `insert into user_data_table (user_email, show_id) values ($1,$2)`,
+    values: [user_data, show_id],
+  };
+  let query_params = pool.query(insert_query, (err, res) => {
+    if (err) {
+      console.log(err.stack);
+    }
   });
+  resp.send('post request received from node');
+});
