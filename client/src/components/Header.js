628f216908a8a5f5ff70e45e0da1f2a791871229
Ivan Feng <feng.ivan.1235@gmail.com>
2021-07-31 00:09:39 -0400

    Small update to header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -6,7 +6,7 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
-import {Link, NavLink } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -18,10 +18,10 @@ const useStyles = makeStyles((theme) => ({
   title: {
     flexGrow: 1,
   },
-  link : {
-    color: "#FFF",
-    textDecoration: "none"
-  }
+  link: {
+    color: '#FFF',
+    textDecoration: 'none',
+  },
 }));
 
 export default function ButtonAppBar() {
@@ -31,18 +31,26 @@ export default function ButtonAppBar() {
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
           <Typography variant="h6" className={classes.title}>
-            News
+            app
           </Typography>
-          <Button><Link className={classes.link} to="/">Home</Link> </Button>
-          <Button><Link className={classes.link} to="/browse">browse</Link> </Button>
-          <Button><Link className={classes.link} to="/profile">profile</Link> </Button>
+          <Button>
+            <Link className={classes.link} to="/">
+              Home
+            </Link>{' '}
+          </Button>
+          <Button>
+            <Link className={classes.link} to="/browse">
+              browse
+            </Link>{' '}
+          </Button>
+          <Button>
+            <Link className={classes.link} to="/profile">
+              profile
+            </Link>{' '}
+          </Button>
         </Toolbar>
       </AppBar>
     </div>
   );
 }
-
